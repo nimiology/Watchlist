207a8de950f66013699d9fe37fe82829fcbfdb67
nima <nima@gamil.com>
2021-03-02 08:56:49 +0330

    UI better

--- src/main.py
+++ src/main.py
@@ -2,12 +2,8 @@ import json
 import os
 import tkinter
 from tkinter import *
-
-from kivy.config import Config
-from kivy.lang import Builder
-from kivymd.app import MDApp
 from tabulate import _table_formats, tabulate
-from kivy.core.window import Window
+
 
 DIRECT = []
 direct01 = []
@@ -38,7 +34,7 @@ except:
 
 
 def TKINTERSMALL(geometry,title,label,inputwidth,buttontext,X,Y):
-    #add name of folders from directory folder
+    #Tkinter for str
     WINDOW = tkinter.Tk()
     WINDOW.geometry(geometry)
     WINDOW.resizable(False, False)
@@ -60,135 +56,133 @@ def TKINTERSMALL(geometry,title,label,inputwidth,buttontext,X,Y):
     WINDOW.mainloop()
     return TXTBIND
 
-class MainApp(MDApp):
-    def build(self):
-        self.theme_cls.primary_palette = "Lime"
-        self.theme_cls.theme_style = "Dark" 
-        Window.size = (500, 600)
-        return Builder.load_file("UI.kv")
+def DIRECTORY():
+    #in here we give directory after that this will get
+    #name of folders in directory
+    try:
+        TXT = TKINTERSMALL("620x70","Directory","Type your series directory:"
+                           ,"100","Confrim","555","27")
+        print(TXT)
+        DIRECT.append(TXT)
+        my_list = os.listdir(DIRECT[len(DIRECT)-1])
 
-    def DIRECTORY(self):
-        #in here we give directory after that this will get
-        #name of folders in directory
-        try:
-            TXT = TKINTERSMALL("620x70","Directory","Type your series directory:"
-                               ,"100","Confrim","555","27")
-            print(TXT)
-            DIRECT.append(TXT)
-            my_list = os.listdir(DIRECT[len(DIRECT)-1])
+        for jojoj in my_list:
+            my_listw.append(jojoj)
+        for emin in my_listw:
+            MAINLIST[emin] = 0
+        save()
+    except:
+        pass
 
-            for jojoj in my_list:
-                my_listw.append(jojoj)
-            for emin in my_listw:
-                MAINLIST[emin] = 0
-            save()
-        except:
-            pass
+def EDIT():
+    #edit serie
+    WINDOW = tkinter.Tk()
+    WINDOW.title("Edit")
+    WINDOW.geometry("920x670+20+5")
 
-    def EDIT(self):
-        #edit serie
+    LISTRUN = LISTFORRUN()
+    KEY = []
+    VALUE = []
+
+    for key, value in LISTRUN.items():
+        VALUE.append(key)
+        KEY.append(value)
+
+    for NUMKEY in range(0, len(KEY)):
+        LABEL = Label(WINDOW, text="[{0}]  {1}  ".format((NUMKEY + 1), VALUE[NUMKEY]))
+        X = "15"
+        if (NUMKEY > 21):
+            X = "500"
+            Y = 10 + (NUMKEY - 22) * 30
+            if (NUMKEY > 43):
+                Y = 10 + (NUMKEY - 44) * 30
+                X = "1000"
+        else:
+            Y = 10 + NUMKEY * 30
+        LABEL.place(x=X, y=Y)
+    BUTTON1 = Button(WINDOW, text="Edit", width="10")
+    BUTTON1.place(x="820", y="620")
+
+    BUTTON2 = Button(WINDOW, text="Delete", width="10")
+    BUTTON2.place(x="730", y="620")
+
+    def EDIT(event):
         WINDOW = tkinter.Tk()
-        WINDOW.title("Edit")
-        WINDOW.geometry("920x670+20+5")
+        WINDOW.geometry("200x70")
+        WINDOW.resizable(False, False)
+        WINDOW.title("type")
+        LABEL = Label(WINDOW, text="Write series number:")
+        INPUT = Entry(WINDOW, width="25")
+        BUTTON = Button(WINDOW, text="Confrim")
+        LABEL.grid(row=0)
+        BUTTON.place(x="140", y="25")
+        INPUT.place(x="5", y="30")
 
-        LISTRUN = LISTFORRUN()
-        KEY = []
-        VALUE = []
-
-        for key, value in LISTRUN.items():
-            VALUE.append(key)
-            KEY.append(value)
-
-        for NUMKEY in range(0, len(KEY)):
-            LABEL = Label(WINDOW, text="[{0}]  {1}  ".format((NUMKEY + 1), VALUE[NUMKEY]))
-            X = "15"
-            if (NUMKEY > 21):
-                X = "500"
-                Y = 10 + (NUMKEY - 22) * 30
-                if (NUMKEY > 43):
-                    Y = 10 + (NUMKEY - 44) * 30
-                    X = "1000"
-            else:
-                Y = 10 + NUMKEY * 30
-            LABEL.place(x=X, y=Y)
-        BUTTON1 = Button(WINDOW, text="Edit", width="10")
-        BUTTON1.place(x="820", y="620")
-
-        BUTTON2 = Button(WINDOW, text="Delete", width="10")
-        BUTTON2.place(x="730", y="620")
-
-        def EDIT(event):
-            WINDOW = tkinter.Tk()
-            WINDOW.geometry("200x70")
-            WINDOW.resizable(False, False)
-            WINDOW.title("type")
-            LABEL = Label(WINDOW, text="Write series number:")
-            INPUT = Entry(WINDOW, width="25")
-            BUTTON = Button(WINDOW, text="Confrim")
-            LABEL.grid(row=0)
-            BUTTON.place(x="140", y="25")
-            INPUT.place(x="5", y="30")
-
-            def edit(event):
-                NUMSERIE = int(INPUT.get())-1
-                numserie1 = NUMSERIE
-                try:
-                    seasonupdate = int(TKINTERSMALL("620x70",f"{VALUE[NUMSERIE]}",
-                                                    f"{VALUE[NUMSERIE]} Season ",
-                                                    "100","Confrim","555","27"))
-                    MAINLIST[VALUE[numserie1]] = seasonupdate
-                    save()
-                except:
-                    pass
-
-
-            BUTTON.bind("<ButtonRelease-1>", edit)
-
-        def DELET(event):
+        def edit(event):
+            NUMSERIE = int(INPUT.get())-1
+            numserie1 = NUMSERIE
             try:
-                TEXT = NAMESERIE = TKINTERSMALL("620x70","Delete",
-                                                "Type number of series which you want to delete(Example:1,2,10):",
-                                                "100","Confrim","555","27")
-                SPLITER = TEXT.split(",")
-                for deleter in SPLITER:
-                   del MAINLIST[VALUE[int(deleter) - 1]]
-                   print (MAINLIST)
+                seasonupdate = int(TKINTERSMALL("620x70",f"{VALUE[NUMSERIE]}",
+                                                f"{VALUE[NUMSERIE]} Season ",
+                                                "100","Confrim","555","27"))
+                MAINLIST[VALUE[numserie1]] = seasonupdate
                 save()
             except:
                 pass
 
 
-        BUTTON1.bind("<ButtonRelease-1>", EDIT)
-        BUTTON2.bind("<ButtonRelease-1>", DELET)
+        BUTTON.bind("<ButtonRelease-1>", edit)
 
-        WINDOW.mainloop()
-
-    def TABLE(self):
-        #show table
-        WINDOW = tkinter.Tk()
-        WINDOW.title("Tabel")
-        data = [["Serie Name", "Season"]]
-
-        DICT=LISTFORRUN()
-
-        for key, value in DICT.items():
-            data.append([key,value])
-
-        TABLE=tabulate(data, headers='firstrow', showindex='always', tablefmt='fancy_grid')
-        TEXT = tkinter.Text(WINDOW, height=100, width=1000)
-        TEXT.pack()
-        TEXT.insert(tkinter.END, "{0}".format(TABLE))
-        tkinter.mainloop()
-        WINDOW.mainloop()
-
-    def NEW(self):
-        #in here we add new serie to the table
+    def DELET(event):
         try:
-            NAMESERIE = TKINTERSMALL("620x70","Directory","New series:"
-                                     ,"100","Confrim","555","27")
-            MAINLIST[NAMESERIE] = 0
+            TEXT = NAMESERIE = TKINTERSMALL("620x70","Delete",
+                                            "Type number of series which you want to delete(Example:1,2,10):",
+                                            "100","Confrim","555","27")
+            SPLITER = TEXT.split(",")
+            for deleter in SPLITER:
+               del MAINLIST[VALUE[int(deleter) - 1]]
+               print (MAINLIST)
             save()
         except:
             pass
 
-MainApp().run()
+
+    BUTTON1.bind("<ButtonRelease-1>", EDIT)
+    BUTTON2.bind("<ButtonRelease-1>", DELET)
+
+    WINDOW.mainloop()
+
+def NEW():
+    #in here we add new serie to the table
+    try:
+        NAMESERIE = TKINTERSMALL("620x70","Directory","New series:"
+                                 ,"100","Confrim","555","27")
+        MAINLIST[NAMESERIE] = 0
+        save()
+    except:
+        pass
+
+def TABLE():
+    #show table
+    WINDOW = tkinter.Tk()
+    WINDOW.resizable(False, False)
+    WINDOW.geometry('1100x650')
+    WINDOW.title("Tabel")
+    data = [["Serie Name", "Season"]]
+
+    DICT=LISTFORRUN()
+
+    for key, value in DICT.items():
+        data.append([key,value])
+
+    TABLE=tabulate(data, headers='firstrow', showindex='always', tablefmt='fancy_grid')
+    TEXT = tkinter.Text(WINDOW,height=40, width=70)
+    TEXT.grid(column=0)
+    TEXT.insert(tkinter.END, f"{TABLE}")
+
+    tkinter.mainloop()
+    WINDOW.mainloop()
+
+
+
+TABLE()
