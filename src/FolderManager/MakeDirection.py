7bfc9d8d406f992e743831e85a54b837b9bddb71
Nima <nimiologyy@gmail.com>
2021-07-20 21:16:12 +0430

    Folder Manager

--- src/FolderManager/MakeDirection.py
+++ src/FolderManager/MakeDirection.py
@@ -1,1 +1,14 @@
 import os
+
+def mkdir(Directory, Name, Seasons, icon=''):
+    Parent_Folder = f"{Directory}/{Name}"
+    try:
+        os.mkdir(Parent_Folder)
+    except:
+        print(f"[Error] '{Name}' already exists!")
+    for i in range(Seasons):
+        Child_Folder = f'{Parent_Folder}/{Name} {i + 1}'
+        try:
+            os.mkdir(Child_Folder)
+        except:
+            print(f"[Error] '{Name}/{Name} {i + 1}' already exists!")
